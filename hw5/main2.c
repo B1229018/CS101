1c971cfe907cfa04f2d6b3645ed93b9b92d1db0f
B1229018 <144312577+B1229018@users.noreply.github.com>
2023-12-19 10:06:19 +0800

    Create main2.c

---
+++ hw5/main2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int PI;
+    int a=1,i;
+    for(i=1;i<=20000;i++){
+        PI+=400000/((2*i-1)*a);
+        printf("%d\n",PI);
+        a*=-1;
+        if(PI==314159){
+            printf("%d",2*i-1);
+            break;
+        }
+    }
+
+
+    return 0;
+}
