198782fe13c68bbbb101e86e2b7f2035f65b4cc6
B1229018 <144312577+B1229018@users.noreply.github.com>
2023-12-20 14:29:50 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-    int PI;
+    float PI;
     int a=1,i;
     for(i=1;i<=20000;i++){
         PI+=400000/((2*i-1)*a);
-        printf("%d\n",PI);
+        printf("%.5f\n",PI/100000);
         a*=-1;
         if(PI==314159){
             printf("%d",2*i-1);
