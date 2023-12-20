e6c00e8195799dd44245efb5de910b1ef7df748b
B1229018 <144312577+B1229018@users.noreply.github.com>
2023-12-20 14:55:05 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -4,14 +4,13 @@ int main()
 {
     int n = 7;
     int k, i;
-    for (k=0 ; k<n ; k++ ){
+    for (k=0 ; k<7 ; k++ ){
         printf("%*d",n,8-n);
         for (i=1 ; i<8 - n ; i++){
             printf("%*d",2,8-n);
         }
         printf("\n");
         n--;
-        k--;
     }
 
 
